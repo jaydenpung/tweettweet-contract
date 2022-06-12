fa2ed87f46f16d43e976612d73daa40bd49bcd6f
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2022-06-12 11:10:38 +0800

    Like Tweet will transfer ether to tweet author

--- contracts/TweetTweet.sol
+++ contracts/TweetTweet.sol
@@ -9,11 +9,14 @@ contract TweetTweet {
     event NewTweet(address indexed from, string message, uint timestamp);
 
     struct Tweet {
+        uint id;
         address author;
         string message;
         uint timestamp;
+        uint likeCount;
     }
 
+    uint tweetIdCounter;
     Tweet[] public tweets;
 
     constructor() payable {
@@ -23,10 +26,25 @@ contract TweetTweet {
     function addTweet(string memory _message) public payable {
         require(msg.value == 0.0001 ether, "Need to pay 0.0001 ether to tweet!");
 
-        Tweet memory tweet = Tweet(msg.sender, _message, block.timestamp);
+        Tweet memory tweet = Tweet(tweetIdCounter++, msg.sender, _message, block.timestamp, 0);
         tweets.push(tweet);
 
         emit NewTweet(tweet.author, tweet.message, tweet.timestamp);
+    }
+
+    function likeTweet(uint tweetId) public payable {
+        require(msg.value == 1000 wei, "Need to pay 1000 wei to like a tweet!");
+        Tweet storage selectedTweet = tweets[tweetId];
+
+        // if like count reach max, we don't want it to overflow and fail
+        if (selectedTweet.likeCount < type(uint256).max) {
+            selectedTweet.likeCount++;
+        }
+
+        address targetWallet = selectedTweet.author;
+        (bool success, ) = (targetWallet).call{value: 1000}("");
+
+        require(success, "Failed to transfer amount to author!");
     }
 
     function getAllTweets() public view returns (Tweet[] memory) {
--- test/tweet.js
+++ test/tweet.js
@@ -4,14 +4,22 @@ const { ethers } = require("hardhat");
 // refer to https://hardhat.org/tutorial/testing-contracts
 
 let owner;
+let randomOne;
 let tweetContract;
+const tweetPaymentAmountInWei = ethers.utils.parseEther("0.0001");
+const likeTweetPaymentAmountInWei = ethers.utils.parseUnits("1000", "wei");
 
 // `beforeEach` will run before each test, re-deploying the contract every
 // time. It receives a callback, which can be async.
 beforeEach(async function () {
-    [owner] = await ethers.getSigners();
+    [owner, randomOne] = await ethers.getSigners();
     const tweetContractFactory = await ethers.getContractFactory("TweetTweet");
     tweetContract = await tweetContractFactory.deploy();
+
+    // have some tweets from the start
+    const payment = { value: tweetPaymentAmountInWei };
+    await tweetContract.addTweet("Tweet by owner with id 0!", payment);
+    await tweetContract.connect(randomOne).addTweet("Tweet by randomOne with id 1!", payment);
 });
 
 describe("Tweet Contract", function () {
@@ -20,20 +28,38 @@ describe("Tweet Contract", function () {
             let addTweetTxn = tweetContract.addTweet("Tweeting without payment!");
             expect(addTweetTxn).to.be.revertedWith("Need to pay 0.0001 ether to tweet!");
         });
+
         it("Tweeting with payment", async function () {
             // inital supply
             let initialSupply = await hre.ethers.provider.getBalance(tweetContract.address);
-            const paymentAmount = "0.0001";
-            const paymentAmountInWei = ethers.utils.parseEther(paymentAmount);
-            const payment = { value: paymentAmountInWei };
 
             // tweet and send payment
+            const payment = { value: tweetPaymentAmountInWei };
             addTweetTxn = tweetContract.addTweet("Tweeting with payment!", payment);
             await addTweetTxn;
 
             //balance supply should increase
             let balanceSupply = await hre.ethers.provider.getBalance(tweetContract.address);
-            expect(balanceSupply).equals(initialSupply + paymentAmountInWei, "Total supply did not increase as expected!");
+            expect(balanceSupply).equals(tweetPaymentAmountInWei.add(initialSupply), "Total supply did not increase as expected!");
         })
     });
+
+    describe("Like tweet", function () {
+        it("Like tweet without payment", async function () {
+            let likeTweetTxn = tweetContract.likeTweet(1); // owner liking the second tweet by randomOne
+            expect(likeTweetTxn).to.be.revertedWith("Need to pay 1000 wei to like a tweet!");
+        })
+
+        it("Like tweet with payment", async function () {
+            // get balance of random one before transaction
+            let initialBalance = await hre.ethers.provider.getBalance(randomOne.address);
+
+            const payment = { value: likeTweetPaymentAmountInWei };
+            await tweetContract.likeTweet(1, payment); // owner liking the second tweet by randomOne
+            let balance = await hre.ethers.provider.getBalance(randomOne.address);
+
+            // author of the tweet (randomOne) should get the payment
+            expect(balance).equals(initialBalance.add(likeTweetPaymentAmountInWei));
+        })
+    })
 });
